42266d94242c7939d2cea1b3f6778734357c2e33
theophildio <dtheo.sangma@gmail.com>
2022-05-06 13:48:44 +0600

    Done

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-	res.send("Warehouse server running. All good.");
+	res.send("Warehouse server running.");
 });
 
 app.listen(port, () => {
