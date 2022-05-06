f679a1b64c3b6517f5a32c569b5904e85d00808a
theophildio <dtheo.sangma@gmail.com>
2022-05-06 13:36:33 +0600

    back to main branch

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const res = require("express/lib/response");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mhyxo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const productCollection = client.db("wareHouse").collection("product");
+    // Load all data
+		app.get('/product', async (req, res) => {
+			const query = {};
+			const cursor = productCollection.find(query);
+			const products = await cursor.toArray();
+      res.send(products);
+		});
+    // Load single data
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    })
+	} finally {
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("Warehouse server running. Github error");
+});
+
+app.listen(port, () => {
+	console.log("Listing to port:", port);
+});
